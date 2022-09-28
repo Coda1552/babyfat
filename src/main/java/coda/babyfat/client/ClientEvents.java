65ef75c16f5c344cd8f66ddf402ae2e1d5f388cc
baguchan <shurf012@gmail.com>
2022-09-28 16:42:59 +0900

    don't forget cutout

--- src/main/java/coda/babyfat/client/ClientEvents.java
+++ src/main/java/coda/babyfat/client/ClientEvents.java
@@ -3,7 +3,11 @@ package coda.babyfat.client;
 import coda.babyfat.BabyFat;
 import coda.babyfat.client.model.RanchuModel;
 import coda.babyfat.client.renderer.RanchuRenderer;
+import coda.babyfat.registry.BFBlocks;
 import coda.babyfat.registry.BFEntities;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -13,8 +17,12 @@ import net.minecraftforge.fml.common.Mod;
 public class ClientEvents {
 
 	public static void init() {
+		setRenderLayer(BFBlocks.WATER_LETTUCE.get(), RenderType.cutout());
 	}
 
+	private static void setRenderLayer(Block block, RenderType type) {
+		ItemBlockRenderTypes.setRenderLayer(block, type::equals);
+	}
 	@SubscribeEvent
 	public static void registerLayerDefinition(EntityRenderersEvent.RegisterLayerDefinitions event) {
 		event.registerLayerDefinition(ModModelLayers.RANCHU, RanchuModel::createBodyLayer);
