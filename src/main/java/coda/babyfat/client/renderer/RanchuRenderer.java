15a859a26625796469dafe1f9c0c0eccf5503e18
baguchan <shurf012@gmail.com>
2022-10-06 13:20:51 +0900

    ranchu spawn variant crash fixed *again*

--- src/main/java/coda/babyfat/client/renderer/RanchuRenderer.java
+++ src/main/java/coda/babyfat/client/renderer/RanchuRenderer.java
@@ -6,6 +6,7 @@ import coda.babyfat.client.model.RanchuModel;
 import coda.babyfat.common.entities.Ranchu;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Vector3f;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.resources.ResourceLocation;
@@ -16,6 +17,7 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 @OnlyIn(Dist.CLIENT)
 public class RanchuRenderer<T extends Ranchu> extends MobRenderer<T, RanchuModel<T>> {
 	private static final ResourceLocation[] TEXTURES = new ResourceLocation[Ranchu.MAX_VARIANTS];
+	private static final ResourceLocation DEFAULT_TEXTURES = new ResourceLocation(BabyFat.MOD_ID, "textures/entity/ranchu/ranchu_1.png");
 
 	public RanchuRenderer(EntityRendererProvider.Context context) {
 		super(context, new RanchuModel<>(context.bakeLayer(ModModelLayers.RANCHU)), 0.35F);
@@ -24,13 +26,18 @@ public class RanchuRenderer<T extends Ranchu> extends MobRenderer<T, RanchuModel
 	@Override
 	public ResourceLocation getTextureLocation(T entity) {
 		int variant = entity.getVariant() + 1;
-		ResourceLocation loc = new ResourceLocation(BabyFat.MOD_ID, "textures/entity/ranchu/ranchu_" + variant + ".png");
-		if(variant > 303){
-			ResourceLocation loc2 = new ResourceLocation(BabyFat.MOD_ID, "textures/entity/ranchu/ranchu_1.png");
+		if (TEXTURES[variant - 1] == null) {
+			ResourceLocation loc = new ResourceLocation(BabyFat.MOD_ID, "textures/entity/ranchu/ranchu_" + variant + ".png");
+			if (!Minecraft.getInstance().getResourceManager().getResource(loc).isPresent()) {
+				System.out.println("Found Unknown variant " + variant + ", using default");
+				loc = DEFAULT_TEXTURES;
+				return loc;
+			}
 
-			return loc2;
+			return TEXTURES[variant - 1] = loc;
 		}
-		return TEXTURES[variant] = loc;
+
+		return TEXTURES[variant - 1];
 	}
 
 	@Override
